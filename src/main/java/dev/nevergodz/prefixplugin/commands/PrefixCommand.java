3702678cae74d00ac438e8a5431507e196253242
nevergodz <neverovh@gmail.com>
2023-10-02 17:30:55 +0100

    test

--- src/main/java/dev/nevergodz/prefixplugin/commands/PrefixCommand.java
+++ src/main/java/dev/nevergodz/prefixplugin/commands/PrefixCommand.java
@@ -25,14 +25,17 @@ public class PrefixCommand implements CommandExecutor {
 
         if (command.getName().equalsIgnoreCase("prefix")) {
             if (args.length == 0) {
-                prefixManager.listPrefixes(player);
+                player.sendMessage("Используйте:");
+                player.sendMessage("/prefix list - для просмотра ваших префиксов.");
+                player.sendMessage("/prefix add <ник/UUID> <префикс> - для добавления префикса.");
+                player.sendMessage("/prefix remove <ник/UUID> <префикс> - для удаления префикса.");
             } else if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
-                prefixManager.listPrefixes(player);
+                prefixManager.listPrefixes(player); // Обработка команды /prefix list
             } else if (args.length == 3) {
                 if (args[0].equalsIgnoreCase("add")) {
                     String targetName = args[1];
                     String prefixId = args[2];
-                    prefixManager.setTabSuffix(player, targetName, prefixId);
+                    prefixManager.addPrefix(player, targetName, prefixId);
                 } else if (args[0].equalsIgnoreCase("remove")) {
                     String targetName = args[1];
                     String prefixId = args[2];
@@ -48,5 +51,4 @@ public class PrefixCommand implements CommandExecutor {
         }
         return false;
     }
-
 }
--- src/main/java/dev/nevergodz/prefixplugin/managers/PrefixManager.java
+++ src/main/java/dev/nevergodz/prefixplugin/managers/PrefixManager.java
@@ -1,31 +1,43 @@
 package dev.nevergodz.prefixplugin.managers;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.PacketContainer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 public class PrefixManager {
 
     private final JavaPlugin plugin;
     private final FileConfiguration config;
+    private final Map<Player, String> playerPrefixes = new HashMap<>();
 
     public PrefixManager(JavaPlugin plugin) {
         this.plugin = plugin;
         this.config = plugin.getConfig();
     }
 
-    public void setPlayerPrefix(Player player, String prefix) {
-        // Установка суффикса в табе
-        setTabSuffix(player, prefix);
+    public void setPlayerListName(Player player, String displayName) {
+        player.setPlayerListName(displayName);
+    }
 
-        // Установка префикса над головой
-        sendPrefixAboveHead(player, prefix);
+    public String findPrefixIdByName(String name) {
+        Set<String> prefixIds = config.getConfigurationSection("prefixes").getKeys(false);
+
+        for (String prefixId : prefixIds) {
+            String prefixName = config.getString("prefixes." + prefixId + ".name");
+            if (prefixName != null && prefixName.equalsIgnoreCase(name)) {
+                return prefixId;
+            }
+        }
+
+        return null;
     }
 
     public void listPrefixes(Player player) {
@@ -44,51 +56,69 @@ public class PrefixManager {
         }
     }
 
-    public void removePrefix(Player sender, Player target, String prefixId) {
-        // Удаление префикса у игрока
-        removePlayerPrefix(target);
+    public void addPrefix(Player sender, String targetName, String name) {
+        String prefixId = findPrefixIdByName(name);
 
-        sender.sendMessage("Префикс с ID: " + prefixId + " успешно удален у " + target.getName());
-    }
-
-    private void setTabSuffix(Player player, String suffix) {
-        PacketContainer packet = ProtocolLibrary.getProtocolManager()
-                .createPacket(PacketType.Play.Server.SCOREBOARD_TEAM);
-
-        // Устанавливаем имя команды (ник игрока)
-        packet.getStrings().write(0, player.getName());
-        // Устанавливаем суффикс для отображения в табе
-        packet.getStrings().write(2, suffix);
-
-        try {
-            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+        if (prefixId == null) {
+            sender.sendMessage("Префикс " + name + " не найден.");
+            return;
         }
-    }
 
-    private void sendPrefixAboveHead(Player player, String prefix) {
-        // Отправка пакета для отображения префикса над головой
-        PacketContainer packet = ProtocolLibrary.getProtocolManager()
-                .createPacket(PacketType.Play.Server.SCOREBOARD_TEAM);
+        Player target = plugin.getServer().getPlayer(targetName);
 
-        // Устанавливаем уникальное имя команды (нужно для разных префиксов у разных игроков)
-        packet.getStrings().write(0, player.getUniqueId().toString());
-        // Устанавливаем название команды (для отображения префикса)
-        packet.getStrings().write(2, prefix);
-
-        try {
-            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
+        if (target == null) {
+            sender.sendMessage("Игрок " + targetName + " не найден.");
+            return;
         }
+
+        String currentPrefixes = playerPrefixes.getOrDefault(target, "");
+        if (!currentPrefixes.isEmpty()) {
+            currentPrefixes += ",";
+        }
+        currentPrefixes += prefixId;
+        playerPrefixes.put(target, currentPrefixes); // Добавьте эту строку
+
+        String displayName = config.getString("prefixes." + prefixId + ".displayName");
+        target.setPlayerListName(target.getName() + " " + displayName);
+
+        sender.sendMessage("Префикс " + name + " установлен для " + target.getName());
     }
 
-    private void removePlayerPrefix(Player player) {
-        // Удаляем суффикс в табе
-        setTabSuffix(player, "");
 
-        // Отправляем пустой префикс над головой
-        sendPrefixAboveHead(player, "");
+    public void removePrefix(Player sender, String targetName, String prefixId) {
+        Player target = plugin.getServer().getPlayer(targetName);
+
+        if (target == null) {
+            sender.sendMessage("Игрок " + targetName + " не найден.");
+            return;
+        }
+
+        String currentPrefixes = playerPrefixes.getOrDefault(target, "");
+        String[] prefixes = currentPrefixes.split(",");
+
+        boolean removed = false;
+        StringBuilder newPrefixes = new StringBuilder();
+        for (String playerPrefix : prefixes) {
+            if (!playerPrefix.equals(prefixId)) {
+                if (newPrefixes.length() > 0) {
+                    newPrefixes.append(",");
+                }
+                newPrefixes.append(playerPrefix);
+            } else {
+                removed = true;
+            }
+        }
+
+        if (!removed) {
+            sender.sendMessage("Игрок " + target.getName() + " не имеет префикса: " + prefixId);
+        } else {
+            // Обновить сообщение в чате с новыми префиксами
+            setPlayerListName(target, target.getName() + " " + newPrefixes.toString());
+
+            sender.sendMessage("Префикс: " + prefixId + " удален у " + target.getName());
+        }
+
+        playerPrefixes.put(target, newPrefixes.toString());
+        setPlayerListName(target, target.getName());
     }
 }
